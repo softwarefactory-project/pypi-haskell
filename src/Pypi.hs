0b4045fc2b84f5170e230a12673d5b6f04b92ce3
Tristan Cacqueray <tdecacqu@redhat.com>
2020-07-28 13:17:03 +0000

    Add initial structure
    Change-Id: I6a647096bb96fb67ff9517f8a8b46d039da5187c

--- src/Pypi.hs
+++ src/Pypi.hs
@@ -1,8 +1,59 @@
 -- | This module contains the pypi REST client
 module Pypi
-  ( getVersion,
+  ( -- * Client
+    PypiClient (baseUrl),
+    withClient,
+
+    -- * Api
+    getProject,
+
+    -- * Main data types
+    PypiProject (..),
   )
 where
 
-getVersion :: String -> [String]
-getVersion _ = ["0.0.0"]
+import Data.Aeson (FromJSON, decode, eitherDecode)
+import Data.Maybe (fromJust)
+import qualified Data.Text as T
+import Data.Text (Text, unpack)
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Pypi.Project
+
+-- | The PypiClient record, use 'withClient' to create
+data PypiClient
+  = PypiClient
+      { -- | the base url
+        baseUrl :: Text,
+        manager :: Manager
+      }
+
+-- | Create the 'PypiClient'
+withClient ::
+  -- | The callback
+  (PypiClient -> IO ()) ->
+  -- | withClient performs the IO
+  IO ()
+withClient callBack =
+  do
+    manager <- newManager tlsManagerSettings
+    callBack (PypiClient {..})
+  where
+    baseUrl = "https://pypi.org/pypi/"
+
+pypiGet ::
+  (FromJSON a) =>
+  Text ->
+  PypiClient ->
+  IO a
+pypiGet path PypiClient {..} =
+  do
+    request <- parseUrlThrow (unpack $ baseUrl <> path)
+    response <- httpLbs request manager
+    case eitherDecode $ responseBody response of
+      Left err -> error $ "Decoding of " <> show (responseBody response) <> " failed with: " <> err
+      Right a -> pure a
+
+-- | Get project infos
+getProject :: Text -> PypiClient -> IO PypiProject
+getProject project = pypiGet (project <> "/json")
