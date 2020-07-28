a12f7275ef238212381c67f8a7a4f9dce23b0176
Tristan Cacqueray <tdecacqu@redhat.com>
2020-07-28 13:44:18 +0000

    Add getReleaseSemVer function
    Change-Id: I8925bbf4aa9f9317d69e88c48ba659f26458fc01

--- src/Pypi/Project.hs
+++ src/Pypi/Project.hs
@@ -3,15 +3,21 @@
 -- | The project data type
 module Pypi.Project
   ( PypiProject (..),
+    PypiProjectInfo (..),
+    PypiProjectRelease (..),
+    getReleaseSemVer,
   )
 where
 
 import Data.Aeson (FromJSON, Options (fieldLabelModifier), defaultOptions, genericParseJSON, parseJSON)
 import Data.Char (isUpper, toLower)
+import Data.Either (rights)
 import qualified Data.Map as M
-import Data.Maybe (fromJust, fromMaybe, isJust)
+import Data.Maybe (fromJust, fromMaybe, isJust, catMaybes, mapMaybe)
 import Data.Text (Text)
 import qualified Data.Text as T
+import qualified Data.Versions
+import Data.Versions (SemVer, versioning)
 import GHC.Generics (Generic)
 
 data PypiProjectInfo
@@ -36,6 +42,15 @@ data PypiProject
         ppReleases :: M.Map Text [PypiProjectRelease]
       }
   deriving (Eq, Show, Ord, Generic)
+
+-- Get release semver
+getReleaseSemVer :: PypiProject -> [SemVer]
+getReleaseSemVer = mapMaybe go . rights . map versioning . M.keys . ppReleases
+  where
+    go :: Data.Versions.Versioning -> Maybe SemVer
+    go v = case v of
+      Data.Versions.Ideal semver -> Just semver
+      _ -> Nothing
 
 -- Convert ppiAuthorEmail to author_email
 pypiParseJSON :: String -> Options
