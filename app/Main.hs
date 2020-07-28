0863f4eb3da3cc16212eff3336856189dc0139da
Tristan Cacqueray <tdecacqu@redhat.com>
2020-07-28 13:44:18 +0000

    Add getReleaseSemVer function
    Change-Id: I8925bbf4aa9f9317d69e88c48ba659f26458fc01

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,21 @@
 module Main (main) where
 
-import Pypi (withClient, getProject)
+import System.Environment (getArgs)
+import qualified Data.Text as T
+import Control.Monad (forM_)
+import Pypi (withClient, getProject, getReleaseSemVer)
+
 
 main :: IO ()
-main = withClient $ \client -> do
-  project <- getProject "ansible" client
-  print project
+main = do
+  args <- getArgs
+  case args of
+    [project] -> printSemVers (T.pack project)
+    _ -> putStrLn "usage: pypi-cli project-name"
+
+printSemVers :: T.Text -> IO ()
+printSemVers projectName =
+  withClient $ \client -> do
+    project <- getProject projectName client
+    print $ projectName <> " versions:"
+    forM_ (getReleaseSemVer project) print
